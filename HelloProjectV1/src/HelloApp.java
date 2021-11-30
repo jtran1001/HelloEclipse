453fa93e0ffe96ec27c524b7a42dc0955b509dcc
jtran1001 <dtran@unb.ca>
2021-11-30 14:15:03 -0400

    Ver3

--- HelloProjectV1/src/HelloApp.java
+++ HelloProjectV1/src/HelloApp.java
@@ -4,6 +4,7 @@ public class HelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Tran");
+		System.out.println("This is to activate second change");
 	}
 
 }
